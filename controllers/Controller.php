9b0051edd011160d20a2a60f70336e7defb9d72c
ugrcngz35 <ugurcengiztr35@gmail.com>
2019-06-06 21:52:23 +0300

    Add files via upload

---
+++ controllers/Controller.php
@@ -0,0 +1,85 @@
+<?php
+
+
+class Controller extends Model
+{
+
+	public static function loadview($view, $payload = []){
+		if(file_exists("../views/" . $view . ".php")){
+			require "../views/" . $view . ".php";
+		}
+	}
+
+	public function view($view, $payload = []){
+		$view = strtr($view, '.', '/');
+		if(file_exists("../views/" . $view . ".php")){
+			require "../views/" . $view . ".php";
+		}
+	}
+
+	/** Validator for Request */
+	public function validate($requiredFields, $request){
+		return !array_diff_key(array_flip($requiredFields), $request);
+	}
+
+
+	/**
+	 * Redirect homeuri
+	 */
+	public function toBaseUri()
+	{
+		header('Location: /');
+		return false;
+	}
+
+	/**
+	 * Redirect to target
+	 * 
+	 * Url variable is uri or another website address. 
+	 * If isExternal is true, url you enter considered as another web address
+	 *
+	 * @param $url, $isExternal
+	 */
+	public function redirect($url = false, $isExternal = false)
+	{
+		if($url){
+			if($isExternal){
+				header("Location: http://www.$url");
+			}else{
+				header("Location: $url");
+			}
+		}else{
+			die("REDIRECT URL BOŞ BIRAKILMAMALI!");
+		}
+	}
+
+	/**
+	 * Classcaller
+	 * 
+	 * Even though you are running a controller with a uri, 
+	 * you can run another controller class and methods with arguments
+	 * 
+	 * @param $class, $method, $args
+	 */
+	public function callController($class = false, $method = false, Array $args = array())
+	{
+		if($class != false && $method != false){
+			call_user_func_array([new $class, $method], $args);
+		} else if($class != false && $method == false){
+			call_user_func_array([new $class, 'index'], $args);
+		}else {
+			die('callController methodunda eksik argumanlar var!');
+		}
+	}
+
+	/** 
+	 * API CONTROLLER
+	 *
+	 */
+
+
+
+
+}
+
+?>
